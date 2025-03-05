5a2878c2cdea3998589a620e0aaa792b7f7543cc
Prakash Raut <prakash_raut@outlook.com>
2025-03-05 12:44:33 +0530

    add: shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<section>
-			<h1>Hello World!</h1>
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+				Hello World
+			</h1>
+			<Button variant="outline">Click me!</Button>
 		</section>
 	);
 }
